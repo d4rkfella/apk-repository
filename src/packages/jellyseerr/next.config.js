2033d3c47866a796e435e3151907d5b0837785ef
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-04-13 12:54:23 +0300

    Create next.config.js

---
+++ src/packages/jellyseerr/next.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const crypto = require("crypto");
+const webpack = require("webpack");
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+  hooks: {
+    readPackage: (pkg) => {
+      const sortObject = (obj) => Object.keys(obj).sort().reduce((acc, key) => {
+        acc[key] = obj[key];
+        return acc;
+      }, {});
+      
+      pkg.dependencies = sortObject(pkg.dependencies);
+      pkg.peerDependencies = sortObject(pkg.peerDependencies);
+      return pkg;
+    }
+  },
+  generateBuildId: () => 'fixed',
+  outputFileTracing: true,
+  env: {
+    commitTag: process.env.COMMIT_TAG || 'local',
+  },
+  images: {
+    remotePatterns: [
+      { hostname: 'gravatar.com' },
+      { hostname: 'image.tmdb.org' },
+      { hostname: 'artworks.thetvdb.com' },
+      { hostname: 'plex.tv' },
+    ],
+  },
+  webpack(config) {
+    config.output.hashFunction = 'xxhash64';
+    config.optimization.moduleIds = 'deterministic';
+    config.optimization.chunkIds = 'deterministic';
+    config.plugins.push(new webpack.ids.DeterministicModuleIdsPlugin({
+      maxLength: 5,
+      salt: crypto.createHash('sha256').update('fixed').digest('hex')
+    }));
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  experimental: {
+    prerenderManifest: {
+      version: 3,
+      routes: {},
+      dynamicRoutes: {},
+      notFoundRoutes: [],
+      preview: {
+        previewModeId: 'fixed-preview-id',
+        previewModeSigningKey: crypto.createHash('sha256').update('fixed-key').digest('hex')
+      }
+    },
+    scrollRestoration: true,
+    largePageDataBytes: 256000,
+  },
+};
