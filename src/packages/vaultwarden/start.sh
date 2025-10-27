47ff4bdc276177d08c836b74fe428684fb0951f1
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-10-27 10:22:11 +0200

    Create start.sh

---
+++ src/packages/vaultwarden/start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -n "${UMASK}" ]; then
+    umask "${UMASK}"
+fi
+
+if [ -r /etc/vaultwarden.sh ]; then
+    . /etc/vaultwarden.sh
+elif [ -r /etc/bitwarden_rs.sh ]; then
+    echo "### You are using the old /etc/bitwarden_rs.sh script, please migrate to /etc/vaultwarden.sh ###"
+    . /etc/bitwarden_rs.sh
+fi
+
+if [ -d /etc/vaultwarden.d ]; then
+    for f in /etc/vaultwarden.d/*.sh; do
+        [ -r "${f}" ] && . "${f}"
+    done
+elif [ -d /etc/bitwarden_rs.d ]; then
+    echo "### You are using the old /etc/bitwarden_rs.d script directory, please migrate to /etc/vaultwarden.d ###"
+    for f in /etc/bitwarden_rs.d/*.sh; do
+        [ -r "${f}" ] && . "${f}"
+    done
+fi
+
+exec /usr/bin/vaultwarden "${@}"
